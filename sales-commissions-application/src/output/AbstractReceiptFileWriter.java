a868f096be9d5693e74657ff331ce14e036568ab
tsoumpaklas <62931343+tsoumpaklas@users.noreply.github.com>
2023-12-05 22:23:11 +0200

    More changes

--- sales-commissions-application/src/output/AbstractReceiptFileWriter.java
+++ sales-commissions-application/src/output/AbstractReceiptFileWriter.java
@@ -2,30 +2,23 @@ package output;
 
 import java.io.File;
 
+import data.Address;
 import data.Receipt;
 
 
 public abstract class AbstractReceiptFileWriter {
 
 	protected File fileToWrite;
+	/*Receipt contains: receiptID, date, sales,
+	  items, company, kind */
 	protected Receipt receipt;
-	protected String country;
-	protected String city;
-	protected String street;
-	protected String number;
+	/*Address contains: country, city, street, streetNumber
+	  and phoneNumber */
+	protected Address address;
 		
 	public abstract void appendFile();
 	public abstract void setFileToAppend(File fileToAppend); 
-	public abstract void setReceiptID(String receiptID);
-	public abstract void setDate(String date);
-	public abstract void setKind(String kind);
-	public abstract void setSales(String sales);
-	public abstract void setItems(String items);
-	public abstract void setCompany(String company);
-	public abstract void setCountry(String country); 
-	public abstract void setCity(String city);
-	public abstract void setStreet(String street);
-	public abstract void setNumber(String number);
-
+	public abstract void setReceipt(Receipt receipt);
+	public abstract void setA  vddress(Address address);
 }
 
