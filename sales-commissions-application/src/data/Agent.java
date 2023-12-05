8b05abb50677e7d5fc63ea41aa53f9163d7fb45f
tsoumpaklas <62931343+tsoumpaklas@users.noreply.github.com>
2023-12-05 19:04:29 +0200

    Agent class Update

--- sales-commissions-application/src/data/Agent.java
+++ sales-commissions-application/src/data/Agent.java
@@ -16,37 +16,24 @@ public class Agent {
 		allReceipts = new ArrayList<>();
 	}
 	
-	public void setFileType(String fileType) {
-		if(fileType.equals("TXT")){
-			fileAppender = new FileAppenderTXT();
-		}	
-		else{
-			fileAppender = new FileAppenderXML();
-		}	
-	}
-	public List<Receipt> getReceipts(){
-		return allReceipts;
+	public float calculateSalesForEachItem(String kind) {
+		float sumOfSpecificKind = 0;
+		for(int i = 0; i< allReceipts.size(); i++){
+			if(allReceipts.get(i).getKind().equals(kind)){
+				sumOfSpecificKind += allReceipts.get(i).getItems();
+			}
+		}
 
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAfm() {
-		return afm;
-	}
-	public void setAfm(String afm) {
-		this.afm = afm;
+		return sumOfSpecificKind;
 	}
 	
 
-	/*public double calculateTotalSales(){
+	public double calculateTotalSales(){
 		double sumSales = 0;
 		for(int i = 0; i< allReceipts.size(); i++){
 			sumSales += allReceipts.get(i).getSales();
 		}
+
 		return sumSales;
 	}
 	
@@ -56,49 +43,25 @@ public class Agent {
 		for(int i = 0; i< allReceipts.size(); i++){
 			sumItems += allReceipts.get(i).getItems();
 		}
+
 		return sumItems;
 	}
-	
-	public float calculateSkirtsSales(){
-		float skirtSum = 0;
-		for (int i = 0; i< allReceipts.size(); i++){
-			if(allReceipts.get(i).getKind().equals("Skirt")){
-				skirtSum += allReceipts.get(i).getItems();
-			}
-		}
-		return skirtSum;
-	}
 
-	public float calculateCoatsSales(){
-		float coatsSum = 0;
-		for (int i = 0; i< allReceipts.size(); i++){
-				if(allReceipts.get(i).getKind().equals("Coat")){
-					coatsSum += allReceipts.get(i).getItems();
-				}
-		}		
-		return coatsSum;
-	}
-	
-	public float calculateTrouserSales(){
-		float trousersSum = 0;
-		for (int i = 0; i< allReceipts.size(); i++){
-			if(allReceipts.get(i).getKind().equals("Trouser")){
-				trousersSum += allReceipts.get(i).getItems();
-			}	
+	public double calculateCommission(int totalSales) {
+		if (totalSales > 6000 && totalSales <= 10000) {
+			return 0.1 * (totalSales - 6000);
 		}
-		return trousersSum;
-	}
-	
-	public float calculateShirtsSales(){
-		float shirtSum = 0;
-		for (int i = 0; i< allReceipts.size(); i++){
-			if(allReceipts.get(i).getKind().equals("Shirt")){				
-				shirtSum += allReceipts.get(i).getItems();
-			}
+	    else if (totalSales > 10000 && totalSales <= 40000) {
+			return ((totalSales - 10000) * 0.15) + (10000 * 0.1);
+		} 	
+		else if (totalSales > 40000) {
+			return 10000 * 0.1 + 30000 * 0.15 + (totalSales - 40000) * 0.2;
 		}
-		return shirtSum;
+
+		return 0;
 	}
-	
+	/* 
+
 	public double calculateCommission(){
 		double commission = 0;
 		if( this.calculateTotalSales() > 6000 && this.calculateTotalSales()<= 10000){
@@ -113,11 +76,37 @@ public class Agent {
 		return commission;
 	}
 	 */
+	public void setFileType(String fileType) {
+		if(fileType.equals("TXT")){
+			fileAppender = new FileAppenderTXT();
+		}	
+		else{
+			fileAppender = new FileAppenderXML();
+		}	
+	}
 
+	public List<Receipt> getReceipts() {
+		return allReceipts;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAfm() {
+		return afm;
+	}
+
+	public void setAfm(String afm) {
+		this.afm = afm;
+	}
 
 	public FileAppender getFileAppender() {
 		return fileAppender;
 	}
-
 
 }
