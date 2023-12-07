aa7eb50084e27c8773a844ad16a03d7c3d9fda0d
tsoumpaklas <62931343+tsoumpaklas@users.noreply.github.com>
2023-12-07 16:15:52 +0200

    XMLReceiptFileWriter done(or i think so)

--- sales-commissions-application/src/output/XMLReceiptFileWriter.java
+++ sales-commissions-application/src/output/XMLReceiptFileWriter.java
@@ -1,12 +1,7 @@
 package output;
 
-import java.io.File;
-import java.io.IOException;
-
-import javax.swing.text.InternationalFormatter;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
@@ -17,87 +12,61 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
-import data.Address;
-import data.Receipt;
+
 
 public class XMLReceiptFileWriter  extends AbstractReceiptFileWriter{
-
+	private Document document;
+	 
 	protected void createFile() {
-		//creating a document doc
-		try{
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-			Document doc = docBuilder.newDocument();	
-			
-			//creating the root element
-			Element rootElement = doc.createElement("Receipt");
-			doc.appendChild(rootElement);
-			
-			//creating the receiptID element
-			Element receiptID = doc.createElement("ReceiptID");
-			receiptID.appendChild(doc.createTextNode(String.valueOf(receipt.getReceiptID())));
-			rootElement.appendChild(receiptID);
-			
-			//creating the date element
-			Element date = doc.createElement("Date");
-			date.appendChild(doc.createTextNode(receipt.getDate()));
-			rootElement.appendChild(date);
-			
-			//creating the sales element
-			Element sales = doc.createElement("Sales");
-			sales.appendChild(doc.createTextNode(String.valueOf(receipt.getSales())));
-			rootElement.appendChild(sales);
-			
-			//creating the items element
-			Element items = doc.createElement("Items");
-			items.appendChild(doc.createTextNode(String.valueOf(receipt.getItems())));
-			rootElement.appendChild(items);
-			
-			//creating the company element
-			Element company = doc.createElement("Company");
-			rootElement.appendChild(company);
-			
-			//creating the name element
-			Element name = doc.createElement("Name");
-			name.appendChild(doc.createTextNode(receipt.getCompany().getName()));
-			company.appendChild(name);
-			
-			//creating the address element
-			Element address = doc.createElement("Cddress");
-			company.appendChild(address);
-			
-			//creating the country element
-			Element country = doc.createElement("Country");
-			country.appendChild(doc.createTextNode(receipt.getCompany().getAddress().getCountry()));
-			address.appendChild(country);
-			
-			//creating the city element
-			Element city = doc.createElement("City");
-			city.appendChild(doc.createTextNode(receipt.getCompany().getAddress().getCity()));
-			address.appendChild(city);
-			
-			//creating the street element
-			Element street = doc.createElement("street");
-			street.appendChild(doc.createTextNode(receipt.getCompany().getAddress().getStreet()));
-			address.appendChild(street);
-			
-			//creating the streetNumber element
-			Element streetNumber = doc.createElement("streetNumber");
-			streetNumber.appendChild(doc.createTextNode(String.valueOf(receipt.getCompany().getAddress().getStreetNumber())));
-			address.appendChild(streetNumber);
-			
-			//creating the phoneNumber element
-			Element phoneNumber = doc.createElement("phoneNumber");
-			phoneNumber.appendChild(doc.createTextNode(String.valueOf(receipt.getCompany().getAddress().getPhoneNumber())));
+        try {
+			DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+     		DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+    	    document = documentBuilder.parse(fileToWrite);
 
+			Node root = document.getFirstChild();
+            
+
+            Element rootElement = document.createElement("Receipt");
+            root.appendChild(rootElement);
+
+            createElementAndAppend(document, rootElement, "ReceiptID", String.valueOf(receipt.getReceiptID()));
+            createElementAndAppend(document, rootElement, "Date", receipt.getDate());
+			createElementAndAppend(document, rootElement, "Kind", String.valueOf(receipt.getKind()));
+			createElementAndAppend(document, rootElement, "Sales", String.valueOf(receipt.getSales()));
+			createElementAndAppend(document, rootElement, "Items", String.valueOf(receipt.getItems()));
+			createElementAndAppend(document, rootElement, "Company", String.valueOf(receipt.getCompany()));
+			createElementAndAppend(document, rootElement, "Country", String.valueOf(address.getCountry()));
+			createElementAndAppend(document, rootElement, "City", String.valueOf(address.getCity()));
+			createElementAndAppend(document, rootElement, "Street", String.valueOf(address.getStreet()));
+			createElementAndAppend(document, rootElement, "Number", String.valueOf(address.getStreetNumber()));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createElementAndAppend(Document doc, Element parent, String name, String content) {
+      Element element = doc.createElement(name);
+	  element.appendChild(doc.createTextNode(content));
+	  parent.appendChild(element);
+
+	
+}
+
+	@Override
+	protected void writeFile() {
+		try{
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT,"yes");
+			transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+			DOMSource source = new DOMSource(document);
+			
+			StreamResult result = new StreamResult(fileToWrite);
+			transformer.transform(source, result);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		
 	}
-
-
-
-	
-
 }
