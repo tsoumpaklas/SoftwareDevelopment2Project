984a2f0f1f9ed3b3737459dc8d24da250bca5a9a
tsoumpaklas <62931343+tsoumpaklas@users.noreply.github.com>
2023-12-09 00:31:54 +0200

    XML Input

--- sales-commissions-application/src/input/XMLInput.java
+++ sales-commissions-application/src/input/XMLInput.java
@@ -12,45 +12,54 @@ import org.w3c.dom.NodeList;
 
 
 
-public class XMLInput extends Input {
-
-    public void readFile() {
-        try {
-        DocumentBuilderFactory documentBuilderFactory 
-		= DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder
-		= documentBuilderFactory.newDocumentBuilder();
-		Document document = documentBuilder.parse(inputFile);
-        document.getDocumentElement().normalize();
-
+public class XMLInput extends Input {            
+	public void readFile() {
+		Document document = initializeDocument();
 		NodeList nodeList = document.getElementsByTagName("Receipt");
-			for (int i = 0; i < nodeList.getLength(); i++) {
-				Element element = (Element) nodeList.item(i);
-				name = element.getElementsByTagName("Name").item(0).getTextContent();
-				afm = element.getElementsByTagName("Afm").item(0).getTextContent();
-				addAgent();
-				NodeList nodeList2 = element.getElementsByTagName("ReceiptID");
-				for (int j = 0; j < nodeList2.getLength(); j++) {
-					Element element2 = (Element) nodeList2.item(j);
-					receiptID = Integer.parseInt(element2.getTextContent());
-					date = element.getElementsByTagName("Date").item(j).getTextContent();
-					kind = element.getElementsByTagName("Kind").item(j).getTextContent();
-					sales = Integer.parseInt(element.getElementsByTagName("Sales").item(j).getTextContent());
-					items = Integer.parseInt(element.getElementsByTagName("Items").item(j).getTextContent());
-					companyName = element.getElementsByTagName("Company").item(j).getTextContent();
-					companyCountry = element.getElementsByTagName("Country").item(j).getTextContent();
-					companyCity = element.getElementsByTagName("City").item(j).getTextContent();
-					companyStreet = element.getElementsByTagName("Street").item(j).getTextContent();
-					companyStreetNumber = Integer.parseInt(element.getElementsByTagName("Number").item(j).getTextContent());
-					addReceipt();
-				}
+
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Element element = (Element) nodeList.item(i);
+			processAgent(element);
+			NodeList nodeList2 = element.getElementsByTagName("ReceiptID");
+			for (int j = 0; j < nodeList2.getLength(); j++) {
+				Element element2 = (Element) nodeList2.item(j);
+				processReceipt(element2, element, j);
 			}
-            
-        } catch (Exception e) {
-        	JOptionPane.showMessageDialog
-			(null,"�������� ������ �������� ���� �� �������� ��� �������");
-		} 
-    }   
+		}
+	}
+
+	private Document initializeDocument() {
+		Document document = null;
+		try {
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			document = documentBuilder.parse(inputFile);
+			document.getDocumentElement().normalize();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return document;
+	}
+
+	private void processAgent(Element element) {
+		name = element.getElementsByTagName("Name").item(0).getTextContent();
+		afm = element.getElementsByTagName("Afm").item(0).getTextContent();
+		addAgent();
+	}
+
+	private void processReceipt(Element element2, Element element, int j) {
+		receiptID = Integer.parseInt(element2.getTextContent());
+		date = element.getElementsByTagName("Date").item(j).getTextContent();
+		kind = element.getElementsByTagName("Kind").item(j).getTextContent();
+		sales = Integer.parseInt(element.getElementsByTagName("Sales").item(j).getTextContent());
+		items = Integer.parseInt(element.getElementsByTagName("Items").item(j).getTextContent());
+		companyName = element.getElementsByTagName("Company").item(j).getTextContent();
+		companyCountry = element.getElementsByTagName("Country").item(j).getTextContent();
+		companyCity = element.getElementsByTagName("City").item(j).getTextContent();
+		companyStreet = element.getElementsByTagName("Street").item(j).getTextContent();
+		companyStreetNumber = Integer.parseInt(element.getElementsByTagName("Number").item(j).getTextContent());
+		
+	}
 }
 
 
